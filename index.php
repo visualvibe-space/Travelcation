ed4228bccc2e9c133aad7bf2e702bfe3fcaf1038
Divyansh Nanavati <divyanshnanavati08@gmail.com>
2026-02-25 22:35:20 +0530

    fbg

--- index.php
+++ index.php
@@ -934,48 +934,50 @@
         });
     </script>
 
-    <!-- Navigation Bar -->
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top">
-          <div class="container">
-              <a class="navbar-brand" href="index.php">
-                  <img src="uploads/lg-tra (1).png" alt="Travelcation" class="img-fluid">
-              </a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-                  <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarNav">
-                  <ul class="navbar-nav ms-auto">
-                      <li class="nav-item">
-                          <a class="nav-link" href="home.php">Home</a>
-                      </li>
-                      <li class="nav-item">
-                          <a class="nav-link" href="aboutus.php">About</a>
-                      </li>
-                      <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink123" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Explore
+     <!-- Navigation Bar -->
+     <nav class="navbar navbar-expand-lg navbar-light fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">
+                <img src="uploads/lg-tra (1).png" alt="Travelcation" class="img-fluid">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link " href="home.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="aboutus.php">About</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink123"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Explore
                         </a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink123">
-                        <a class="dropdown-item" href="home.php#packages">Packages</a>
-                        <a class="dropdown-item" href="home.php#hotels">Hotels</a>
-                        <a class="dropdown-item" href="offers.php">Exclusive Offers</a>
-                        <a class="dropdown-item" href="alldestinations.php">Destinations</a>
+                            <a class="dropdown-item" href="home.php#packages">Packages</a>
+                            <a class="dropdown-item" href="home.php#hotels">Hotels</a>
+                            <a class="dropdown-item" href="offers.php">Exclusive Offers</a>
+                            <a class="dropdown-item" href="alldestinations.php">Destinations</a>
                         </div>
                     </li>
-                      
-                      <li class="nav-item">
-                          <a class="nav-link" href="home.php#contact-section">Contact</a>
-                      </li>
-                      <li class="nav-item">
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="home.php#contact-section">Contact</a>
+                    </li>
+                    <li class="nav-item">
                         <a class="nav-link" href="submit_feedback.php">Submit Feedback</a>
                     </li>
-                  </ul>
-                  <button class="btn btn-primary ms-lg-3 mt-3 mt-lg-0" data-bs-toggle="modal" data-bs-target="#enquiryModal">
-                      <i class="fas fa-paper-plane me-2"></i>Quick Enquiry
-                  </button>
-              </div>
-          </div>
-      </nav>
+                </ul>
+                <button class="btn btn-primary ms-lg-3 mt-3 mt-lg-0" data-bs-toggle="modal"
+                    data-bs-target="#enquiryModal">
+                    <i class="fas fa-paper-plane me-2"></i>Quick Enquiry
+                </button>
+            </div>
+        </div>
+    </nav>
 
     <!-- Hero Section -->
     <section class="hero-section" id="home">
@@ -1127,6 +1129,13 @@ document.addEventListener('DOMContentLoaded', function() {
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+
+      <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
